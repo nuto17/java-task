02838a0247829e47bc06f2e52cebf7ade7e0e4e0
Danya mac <nuto17@MacBook-Pro-Daniil.local>
2024-12-17 23:16:21 +0300

    add runCli method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,32 @@
 package org.example;
 
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println("Привет " + input);
+        runCli();
+    }
+
+    static String input() {
+        return new Scanner(System.in).nextLine();
+    }
+
+    static void showMenu() {
+        System.out.println("1 - Сказать hello world\n0 - Выход");
+    }
+
+    static void runCli() {
+        while (true) {
+            showMenu();
+            switch (input()) {
+                case "1":
+                    System.out.println("Сказать hello world");
+                    break;
+                case "0":
+                    System.exit(0);
+                default:
+                    System.out.println("неверное вариант");
+            }
+        }
     }
 }
