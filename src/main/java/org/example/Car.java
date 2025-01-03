e76180bbec6675ee0cf4a2ac6ffc3003403ad68a
Danya mac <nuto17@MacBook-Pro-Daniil.local>
2025-01-03 03:32:38 +0300

    i dont know why it doesnt work

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,21 +1,26 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Scanner;
 
 @Data
+@NoArgsConstructor
 public class Car {
-    private String engineType;
     private boolean[] wheels;
+    private Engine engine;
 
-    public Car(String engineType, boolean[] wheels) {
-        this.engineType = engineType;
+    public Car(boolean[] wheels, Engine engine) {
         this.wheels = wheels;
+        this.engine = engine;
     }
 
     public void drive() {
         boolean wheelStatus = true;
+        System.out.println("проверяем двигатель");
+        engine.start();
         for (int i = 0; i < wheels.length; i++) {
             if (!wheels[i]) {
                 System.out.println("колесо" + (i + 1) + "неисправно");
@@ -24,25 +29,15 @@ public class Car {
         }
         if (wheelStatus) {
             System.out.println("все колеса исправны");
-        }
-    }
-
-    public boolean canCarRide() {
-        for (boolean wheel : wheels) {
-            if (!wheel) {
-                return false;
-
-            }
-        }
-        return true;
+        }   engine.stop();//проверка завершена
     }
 
 
-    abstract class Engine implements EngineControl {
+   public abstract static class Engine implements EngineControl {
     }
 
 
-    class PetrolEngine extends Engine implements EngineControl {
+    public static class PetrolEngine extends Engine implements EngineControl {
         @Override
         public void start() {
             System.out.println("Бензиновый двигатель запущен");
@@ -54,7 +49,7 @@ public class Car {
         }
     }
 
-    class ElectricEngine extends Engine implements EngineControl {
+    public static class ElectricEngine extends Engine implements EngineControl {
         @Override
         public void start() {
             System.out.println("электрический двигатель запушен");
--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -1,5 +1,58 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Menu {
-    p
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void showEngineType() {
+        System.out.println("выберете тип двигателя машины: ");
+        System.out.println("1 - бензиновый");
+        System.out.println("2 - электрический");
+        System.out.println("3 - выйти");
+    }
+
+    private static boolean[] setWheels() {
+        boolean[] wheels = new boolean[4];
+        for (int i = 0; i < wheels.length; i++) {
+            wheels[i] = scanner.nextBoolean();
+        }
+        return wheels;
+    }
+    private static boolean[] unlockWheels() {
+        boolean[] wheels = setWheels();
+        boolean[] unlockedWheels = new boolean[wheels.length];
+        System.arraycopy(wheels, 0, unlockedWheels, 0, unlockedWheels.length);
+        return unlockedWheels;
+    }
+
+    public static void showMenu() {
+        Car car = new Car(setWheels(), null);
+        label:
+        {
+            while (true) {
+                showEngineType();
+                int choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("вы выбрали бензиновый двигатель");
+                        car.setEngine(new Car.PetrolEngine());
+                        car.getEngine().start();
+                        car.drive();
+                        break;
+
+                    case 2:
+                        System.out.println("вы выбрали электрический двигатель");
+                        car.setEngine(new Car.ElectricEngine());
+                        car.getEngine().start();
+                        car.drive();
+                        break;
+                    case 3:
+                        break label;
+
+                }
+
+            }
+        }
+    }
+
 }
