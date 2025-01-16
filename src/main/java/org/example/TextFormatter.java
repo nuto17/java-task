279bc75d55abf38cd321d199dc122f6f1c873c01
Danya mac <nuto17@MacBook-Pro-Daniil.local>
2025-01-16 18:02:19 +0300

    zdarova dadya vova

---
+++ src/main/java/org/example/TextFormatter.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TextFormatter {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public enum type {
+        uppercase,
+        lowercase,
+        camelcase,
+        reverse
+    }
+
+    public void chooseType() {
+        System.out.println("Введите текст: ");
+        String text = scanner.nextLine();
+        System.out.println("выбор формата: " + Arrays.toString(type.values()));
+        String choice = scanner.nextLine();
+        switch (choice) {
+            case "uppercase":
+                System.out.println(text.toUpperCase());
+                break;
+            case "lowercase":
+                System.out.println(text.toLowerCase());
+                break;
+            case "camelcase":
+                System.out.println(text.toUpperCase());
+                break;
+            case "reverse":
+                System.out.println(text.toUpperCase());
+                break;
+        }
+
+    }
+}
